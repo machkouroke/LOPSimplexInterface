6cf91c17e0ccfd81c5d588b6e81bcdcd151367a2
machkouroke <machkour20.mo1@gmail.com>
2022-12-03 16:52:52 +0100

    `style` Use of roboto font for documentation

--- src/utils.ts/constant.ts
+++ src/utils.ts/constant.ts
@@ -26,7 +26,7 @@ export const CodeEditorStyle = {
 export const DocsStyle = {
   codeTheme: lightTheme,
   root: {
-    fontFamily: '"Georgia"',
+    fontFamily: '"Roboto"',
     fontSize: 16,
     ...lightTheme.plain,
 
